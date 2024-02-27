95d09755804caba66140c6602eaf274423c67af0
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-02-27 13:47:12 +0600

    after navigation

---
+++ src/components/Body.jsx
@@ -0,0 +1,14 @@
+const Body = () => {
+    return (
+        <div>
+            <div className="text">
+                <p className="tile"> Bangla NLP</p>
+                <p className="i-name">IIT, University of Dhaka</p>
+                <p className="description">Welcome to our NLP Group's website!  We're all about making computers understand our mother language language better. Check out our resources to be part of this journey. Let's shape the future of language and tech together!</p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+const Navigation = () => {
+    return (
+        <nav className="container">
+            <div className="logo">
+                <img src="/images/iitlogo1.png" alt="logo" />
+            </div>
+
+            <ul>
+                <li href="#"> GOAL</li>
+                <li href="#"> PUBLICATIONS</li>
+                <li href="#"> PROJECTS</li>
+                <li href="#"> BLOG</li>
+                <li href="#"> PEOPLE</li>
+                <li href="#"> EVENTS</li>
+            </ul>
+        </nav>
+    );
+};
+
+
+export default Navigation;
