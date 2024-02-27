9075f316e8281a8ed617b56642b0ba580dd9e93d
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-02-27 14:46:19 +0600

    after body

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Navigation from "./components/Navigation"
+import BodyElements from "./components/BodyElements"
 function App() {
   return (
     <div>
       <Navigation/>
+      <BodyElements/>
     </div>
   );
 };
--- src/components/BodyElements.jsx
+++ src/components/BodyElements.jsx
@@ -1,14 +1,17 @@
-const Body = () => {
+const BodyElements = () => {
     return (
-        <div>
+        <div className="body">
             <div className="text">
-                <p className="tile"> Bangla NLP</p>
+                <p className="title"> Bangla   NLP</p>
                 <p className="i-name">IIT, University of Dhaka</p>
                 <p className="description">Welcome to our NLP Group's website!  We're all about making computers understand our mother language language better. Check out our resources to be part of this journey. Let's shape the future of language and tech together!</p>
             </div>
-            <div>
-                
+            <div className="building-image">
+                <img src="/images/iit_du_building1.png" alt="iitdu-building-image"/>
+                <p>IITDU NLP Research Group</p>
             </div>
         </div>
     );
 };
+
+export default BodyElements;
